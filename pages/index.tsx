d9231a2080d8e0281fd3edaacf78420706f42067
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 01:06:26 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
                         </div>
                         <div className="prose max-w-none text-gray-500 dark:text-gray-400">{summary}</div>
                       </div>
-                      </div>
+                    </div>
                 </article>
               </li>
             );
