6afe5043df5d314b8e8d276dd25694d87fc9ad3b
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 00:39:05 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -57,7 +57,6 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
             recent posts
           </h1>
-          <p className="!mt-2 text-lg leading-7 text-gray-500 dark:text-gray-400">{siteMetadata.description}</p>
         </div>
 
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
