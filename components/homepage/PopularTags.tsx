3fa78fa02a296e31c8c287a42b0dd576db52d59e
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-24 22:36:30 +0800

    Update PopularTags.tsx

--- components/homepage/PopularTags.tsx
+++ components/homepage/PopularTags.tsx
@@ -18,7 +18,7 @@ const PopularTags = () => {
         {map(popularTags, (popularTag) => {
           const { slug, iconType, href, title } = popularTag;
 
-          const className = `${slug} flex w-[128px] justify-center space-x-2 rounded-lg p-3`;
+          const className = `${slug} flex w-[150px] justify-center space-x-2 rounded-lg p-3`;
 
           return (
             <Link key={slug} href={href} className={className}>
