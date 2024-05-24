008c04d34076a7a8327fbed0fa768b66232edf9b
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 00:26:00 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,12 @@
+import BuildWith from './BuildWith';
+
 export default function Footer() {
   return (
     <footer>
         <div className="my-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <span>leia mayssa</span>
+          <div>{`Copyright © ${new Date().getFullYear()}`}</div>
+          <span>{` • `}</span>
+          <span>Karhdo's Blog - Karhdo's Coding Adventure</span>
         </div>
     </footer>
   );
