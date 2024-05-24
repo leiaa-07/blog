bdcca4ab68eb50fb01e14f11e64b9378992783fa
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 00:35:57 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,14 @@
+import BuildWith from './BuildWith';
+
 export default function Footer() {
   return (
     <footer>
+      <div className="mb-8 mt-16 items-center justify-between space-y-4 md:mb-10 md:flex md:space-y-0">
+
         <div className="my-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{`leia mayssa`}</div>
         </div>
+      </div>
     </footer>
   );
 }
