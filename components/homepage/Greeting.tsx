1ec79cda27a226f9113b703cb28be886117e2f4d
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 22:31:12 +0800

    Update Greeting.tsx

--- components/homepage/Greeting.tsx
+++ components/homepage/Greeting.tsx
@@ -8,12 +8,7 @@ const Greeting = () => {
 
   return (
     <div className={className}>
-      <div>
-        welcome <span className="font-bold">to leia's blog! :)</span>
-      </div>
-      <p className="text-base text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
-        test
-      </p>
+      welcome <span className="font-bold">to leia's blog! :)</span>
     </div>
   );
 };
