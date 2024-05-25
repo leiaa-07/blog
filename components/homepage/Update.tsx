f63624d5d4ab9c209bd5a032bb74c033a4b2567b
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-26 00:30:11 +0800

    Update Update.tsx

--- components/homepage/Update.tsx
+++ components/homepage/Update.tsx
@@ -1,1 +1,15 @@
+import clsx from 'clsx';
 
+const Update = () => {
+  const className = clsx(
+    'text-2xl font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400 md:text-lg md:leading-7'
+  );
+
+  return (
+    <div className={className}>
+      test
+    </div>
+  );
+};
+
+export default Update;
