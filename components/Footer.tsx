8952e0f091332394d889152b3ef34d535effa1ab
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 18:49:10 +0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,9 +2,10 @@ export default function Footer() {
   return (
     <footer>
       <div className="mb-8 mt-16 items-center justify-between space-y-4 md:mb-10 md:flex md:space-y-0">
-        <div className="my-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
-          <div>{`leia mayssa`}</div>
-        </div>
+        <div>{`leia mayssa`}</div>
+          <div className="my-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
+            <div>{`leia mayssa`}</div>
+          </div>
       </div>
     </footer>
   );
