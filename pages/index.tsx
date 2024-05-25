543f3121dfde14767001e1df318a9b111d0a1c34
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-26 00:19:26 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,12 +49,10 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         <Greeting />
       </div>
 
-      <div className="divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-1 py-2 md:space-y-1">
-          <h1 className="text-2x1 font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
-            test
-          </h1>
-        </div>
+      <div className="space-y-1 py-2 md:space-y-1">
+        <h1 className="text-2x1 font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
+          test
+        </h1>
       </div>
 
       {/* List all post */}
