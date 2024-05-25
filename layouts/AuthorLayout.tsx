94b4f5588a68174e17df7f3a9fdcb76cf4b2604d
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 18:41:04 +0800

    Update AuthorLayout.tsx

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -26,11 +26,8 @@ export default function AuthorLayout({ children, content }: Props) {
       <div className="about divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            About
+            about me
           </h1>
-          <p className="text-base text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
-            Further insights into who I am and the purpose of this blog.
-          </p>
         </div>
 
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
