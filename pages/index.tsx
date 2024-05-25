8d327537813a164ef34ee57ee5abfd0b4761f9cf
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 23:45:13 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -51,9 +51,9 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
 
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 py-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
-            recent posts
-          </h1>
+          <p className="text-base text-gray-500 dark:text-gray-400 md:text-lg md:leading-7">
+            test
+          </p>
         </div>
       </div>
 
