6d6b4379e562ee26c681c24dcb67e7b13dbdcfca
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-25 22:00:13 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -45,7 +45,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
       <PageSEO title={`${headerTitle} - ${title}`} description={description} />
 
       {/* Introduce myself */}
-      <div className="mt-35 dark:divide-gray-700 md:mt-35">
+      <div className="mt-20 dark:divide-gray-700 md:mt-20">
         <Greeting />
       </div>
 
