1671303e88d6ac5fd487f8fc3a0dafcaea3cf66b
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-26 00:32:55 +0800

    Delete components/homepage/Update.tsx

--- components/homepage/Update.tsx
+++
@@ -1,8 +0,0 @@
-  return (
-    <div className=text-2xl font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400 md:text-lg md:leading-7>
-      test
-    </div>
-  );
-};
-
-export default Update;
