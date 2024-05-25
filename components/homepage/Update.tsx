df3d592f6c18b7abbedf8edf87d52889021b4482
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-26 00:31:55 +0800

    Update Update.tsx

--- components/homepage/Update.tsx
+++ components/homepage/Update.tsx
@@ -1,12 +1,5 @@
-import clsx from 'clsx';
-
-const Update = () => {
-  const className = clsx(
-    'text-2xl font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400 md:text-lg md:leading-7'
-  );
-
   return (
-    <div className={className}>
+    <div className=text-2xl font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400 md:text-lg md:leading-7>
       test
     </div>
   );
