bdefe43e44551ab441be2618a94b814ac5f7516d
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-26 19:22:56 +0800

    Update AuthorLayout.tsx

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -36,7 +36,6 @@ export default function AuthorLayout({ children, content }: Props) {
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
 
-          <div className="prose max-w-none pb-8 dark:prose-dark xl:col-span-2">{children}</div>
         </div>
       </div>
     </>
