64b68a1cec4133a2dcb888123e3fad42d580f64b
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-26 19:08:12 +0800

    Update AuthorLayout.tsx

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -13,7 +13,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github } = content;
+  const { name, avatar, occupation, company } = content;
 
   const { title, headerTitle } = siteMetadata;
 
