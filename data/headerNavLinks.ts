1bd8f6b32a1b7299f192f0d2163e5adfd9be6613
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-29 18:35:44 +0800

    Update headerNavLinks.ts

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: 'blog' },
   { href: '/tags', title: 'tags' },
+  { href: '/resources', title: 'resources' },
   { href: '/about', title: 'about me' },
 ];
 
