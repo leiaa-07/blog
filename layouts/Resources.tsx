6155cbabd6841a95f888a22b96f086484706c2af
leiaa-07 <leiamayssa.site@gmail.com>
2024-05-30 16:16:07 +0800

    Create Resources.tsx

---
+++ layouts/Resources.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+import Link from 'next/link';
+import type { Authors } from 'contentlayer/generated';
+
+import siteMetadata from '@/data/siteMetadata';
+import Image from '@/components/Image';
+import { PageSEO } from '@/components/SEO';
+
+interface Props {
+  children: ReactNode;
+  content: Omit<Authors, '_id' | '_raw' | 'body'>;
+}
+
+export default function AuthorLayout({ children, content }: Props) {
+  const { name, avatar, occupation, company, email, twitter, linkedin, github } = content;
+
+  const { title, headerTitle } = siteMetadata;
+
+  const description = 'My professional career, experiences, and skills.';
+
+  return (
+    <>
+      <PageSEO title={`resources - ${headerTitle} - ${title}`} description={description} />
+
+      <div className="about divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            about me
+          </h1>
+        </div>
+
+          <div className="prose max-w-none pb-8 dark:prose-dark xl:col-span-2">{children}</div>
+        </div>
+      </div>
+    </>
+  );
+}
