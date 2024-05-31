384b219c546d578c9a2ce7d8c21617467dcecc26
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-01 01:30:42 +0800

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,15 +1,11 @@
-import { InferGetStaticPropsType } from 'next';
 import { MDXLayoutRenderer } from 'pliny/mdx-components';
 import { MDXComponents } from '@/components/MDXComponents';
-
 import resources from 'data/resources.mdx';
 
 const DEFAULT_LAYOUT = 'Resources';
 
-export const getStaticProps = async () => {
-  return { props: { resources } };
-};
-
-export default function Resources({ resources }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <MDXLayoutRenderer layout={DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />;
+export default function Resources() {
+  return (
+    <MDXLayoutRenderer layout={DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />
+  );
 }
