0a61ef8d2e1d9b4b3415b47f8abc66f02a294bab
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-01 01:07:37 +0800

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,4 +1,3 @@
-// import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { InferGetStaticPropsType } from 'next';
 import { allAuthors } from 'contentlayer/generated';
 import { MDXLayoutRenderer } from 'pliny/mdx-components';
@@ -8,11 +7,13 @@ import { MDXComponents } from '@/components/MDXComponents';
 const DEFAULT_LAYOUT = 'Resources';
 
 export const getStaticProps = async () => {
-  const resources = 'resources';
+  const resources = { layout: 'resources' }; // Create an object with a layout property
 
   return { props: { resources } };
 };
 
 export default function About({ resources }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <MDXLayoutRenderer content={resources} MDXComponents={MDXComponents} />;
+  return (
+    <MDXLayoutRenderer layout={resources.layout || DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />
+  );
 }
