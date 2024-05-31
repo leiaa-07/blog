fb82179d538fa0f586e3f3008fca840165a313d9
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-01 02:02:44 +0800

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,18 +1,21 @@
-// import { MDXLayoutRenderer } from '@/components/MDXComponents'
 import { InferGetStaticPropsType } from 'next';
-import { allAuthors } from 'contentlayer/generated';
 import { MDXLayoutRenderer } from 'pliny/mdx-components';
-
 import { MDXComponents } from '@/components/MDXComponents';
+import resources from 'data/resources.mdx';
 
 const DEFAULT_LAYOUT = 'Resources';
 
 export const getStaticProps = async () => {
-  const author = allAuthors.find((p) => p.slug === 'resources');
-
-  return { props: { author } };
+  // Since 'resources' is an MDX file, we should ensure it's loaded correctly
+  return { props: { content: resources } };
 };
 
-export default function About({ author }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <MDXLayoutRenderer layout={DEFAULT_LAYOUT} content={author} MDXComponents={MDXComponents} />;
+export default function Resources({ content }: InferGetStaticPropsType<typeof getStaticProps>) {
+  return (
+    <MDXLayoutRenderer
+      layout={DEFAULT_LAYOUT}
+      content={content}
+      MDXComponents={MDXComponents}
+    />
+  );
 }
