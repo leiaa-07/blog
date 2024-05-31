6f68905cb7735a2df2e880d5f3736d0e428ae3fc
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-01 02:12:18 +0800

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -5,11 +5,6 @@ import resources from 'data/resources.mdx';
 
 const DEFAULT_LAYOUT = 'Resources';
 
-export const getStaticProps = async () => {
-  // Since 'resources' is an MDX file, we should ensure it's loaded correctly
-  return { props: { content: resources } };
-};
-
 export default function Resources({ content }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return <MDXLayoutRenderer layout={DEFAULT_LAYOUT} content={content} MDXComponents={MDXComponents} />;
+  return <MDXLayoutRenderer layout={DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />;
 }
