e8898b2d634e5ad67237a225126f5831d5446967
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-01 00:57:11 +0800

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,5 +1,4 @@
 // import { MDXLayoutRenderer } from '@/components/MDXComponents'
-import resources from 'data/resources.mdx';
 import { InferGetStaticPropsType } from 'next';
 import { allAuthors } from 'contentlayer/generated';
 import { MDXLayoutRenderer } from 'pliny/mdx-components';
@@ -7,6 +6,9 @@ import { MDXLayoutRenderer } from 'pliny/mdx-components';
 import { MDXComponents } from '@/components/MDXComponents';
 
 const DEFAULT_LAYOUT = 'Resources';
+
+export const getStaticProps = async () => {
+  const resources = allAuthors.find((p) => p.slug === 'resources');
 
   return { props: { resources } };
 };
