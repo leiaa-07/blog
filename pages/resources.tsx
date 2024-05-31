8d65f287c7eff27d27232c476012cb91fd903f83
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-01 01:25:51 +0800

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -1,23 +1,17 @@
 import { InferGetStaticPropsType } from 'next';
-import { allAuthors } from 'contentlayer/generated';
 import { MDXLayoutRenderer } from 'pliny/mdx-components';
-
 import { MDXComponents } from '@/components/MDXComponents';
+
+import resources from 'data/resources.mdx';
 
 const DEFAULT_LAYOUT = 'Resources';
 
 export const getStaticProps = async () => {
-  const resources = allAuthors.find((p) => p.slug === 'resources');
-
-  if (!resources) {
-    return { notFound: true };
-  }
-
   return { props: { resources } };
 };
 
 export default function Resources({ resources }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <MDXLayoutRenderer layout={resources.layout || DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />
+    <MDXLayoutRenderer layout={DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />
   );
 }
