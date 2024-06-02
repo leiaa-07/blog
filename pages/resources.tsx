9a870a7847adb4b17806af05055a60fe5f1f7ce4
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-02 17:10:22 +0800

    Update resources.tsx

--- pages/resources.tsx
+++ pages/resources.tsx
@@ -14,7 +14,6 @@ export const getStaticProps = async () => {
 };
 
 export default function Resources({ resources }: InferGetStaticPropsType<typeof getStaticProps>) {
-  return
-  <MDXLayoutRenderer layout={resources.layout || DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />
-  ;
+  return;
+  <MDXLayoutRenderer layout={resources.layout || DEFAULT_LAYOUT} content={resources} MDXComponents={MDXComponents} />;
 }
