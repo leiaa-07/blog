a16d43622d75037aa4bffd04aed90a28f9927bc6
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-08 03:31:38 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         <Greeting />
       </div>
       <p
-        className="text-2xl font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400"
+        className="sm:text-2xl text-lg font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400"
         style={{ marginTop: '-20px' }}
       >
         current status (26/5): grinding scholarship apps x_x
