bc38c0afab33f2dbee1199cbb8e36378a9e8236a
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-09 02:01:17 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,7 +18,6 @@ import Image from '@/components/Image';
 import Greeting from '@/components/homepage/Greeting';
 import PopularTags from '@/components/homepage/PopularTags';
 
-import { Embed, CommentCount } from 'hyvor-talk-react-typescript';
 import React, {useEffect} from 'react';
 
 const MAX_DISPLAY = 3;
@@ -119,21 +118,3 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
     </div>
   );
 }
-
-const Article: React.FC = () => {
-  return (
-    <div>
-      <h1>Article Title</h1>
-
-      <div className="comment-count-view">
-        {/* Comment Counts */}
-        <CommentCount websiteId={11257} id={''} />
-      </div>
-
-      {/* Load Comments now */}
-      <Embed websiteId={11257} id={''} />
-    </div>
-  );
-};
-
-export { Article };
