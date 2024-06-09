0e82bf6dc19f78744239f3830b15dfc24168bec1
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-10 00:41:27 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,9 @@ function Comments() {
 
   return (
     <div>
-      <h1>Comments Section</h1>
+      <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
+            comments for fun 
+          </h1>
       <div id="comments-container"></div>
     </div>
   );
