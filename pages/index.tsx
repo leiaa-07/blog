d27d8a68265eb9f547aeb9197a0b5f0c9957be06
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-10 01:07:16 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,7 +55,7 @@ function Comments() {
 
   return (
     <div>
-      <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14 border-b border-gray-200 dark:border-gray-700">
+      <h1 className="border-b border-gray-200 text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:border-gray-700 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
         comments for fun
       </h1>
       <div id="comments-container"></div>
