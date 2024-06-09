1aca864dc598ecc620492c0b5eee9c997868cd75
leiaa-07 <leiamayssa.site@gmail.com>
2024-06-10 00:56:22 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -58,6 +58,7 @@ function Comments() {
       <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
         comments for fun
       </h1>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
       <div id="comments-container"></div>
     </div>
   );
