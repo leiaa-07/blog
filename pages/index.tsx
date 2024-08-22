d7bd23cff0b10d9c77386372fe5d8d8c307d6b41
leiaa-07 <leiamayssa.site@gmail.com>
2024-08-22 14:44:32 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         className="text-2xl font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400"
         style={{ marginTop: '-20px' }}
       >
-        current status (20/8): focusing on everything BUT school 😐
+        current status (22/8): 3627 items on my to-do list 😌🤘🏻
       </p>
 
       {/* List all post */}
