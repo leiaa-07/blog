f0cbefb3f108cb898f13b042ab7d4968c4da7b4e
leiaa-07 <leiamayssa.site@gmail.com>
2024-08-22 22:16:39 +0800

    Update AuthorLayout.tsx

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -12,7 +12,7 @@ interface Props {
 }
 
 export default function AuthorLayout({ children, content }: Props) {
-  const { name, avatar, occupation, company, email, twitter, linkedin, github } = content;
+  const { name, avatar, year, occupation, company, email, twitter, linkedin, github } = content;
 
   const { title, headerTitle } = siteMetadata;
 
@@ -32,6 +32,7 @@ export default function AuthorLayout({ children, content }: Props) {
             <Image src={avatar} alt="avatar" width={192} height={192} className="h-48 w-48 rounded-full" />
 
             <h3 className="pb-2 pt-4 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+            <div className="text-gray-500 dark:text-gray-400">{year}</div>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
           </div>
