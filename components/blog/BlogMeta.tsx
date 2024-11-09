ab3b346256f9f6781b6670d2686b8d2e70259c06
leiaa-07 <leiamayssa.site@gmail.com>
2024-11-10 02:58:06 +0800

    Update BlogMeta.tsx

--- components/blog/BlogMeta.tsx
+++ components/blog/BlogMeta.tsx
@@ -14,7 +14,7 @@ const BlogMeta = ({ date, slug, readingTime }: BlogMetaProps) => {
       </time>
       <span className="mx-2">{` • `}</span>
       <div className="flex items-center">
-        <Twemoji emoji="hourglass-not-done" size="1" />
+        ⏳
         <span className="ml-1.5 md:ml-2">{Math.ceil(0.5 * readingTime.minutes)} mins read</span>
       </div>
     </dd>
