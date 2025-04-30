8c4541fb3a041134a150fdf9df40963c6a69157a
leiaa-07 <leiamayssa.site@gmail.com>
2025-04-30 16:47:44 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -89,7 +89,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         className="text-2xl font-medium leading-9 tracking-tight text-gray-500 dark:text-gray-400"
         style={{ marginTop: '-20px' }}
       >
-        current status (14/2): 1.5mths late but HAPPY NEW YEAR 💀
+        current status (30/4): post-happy-events depression 💔
       </p>
 
       {/* List all post */}
