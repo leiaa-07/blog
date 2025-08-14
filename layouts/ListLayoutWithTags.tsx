6785a0660792bcc0fc042039441fa5af87a2590a
leiaa-07 <leiamayssa.site@gmail.com>
2025-08-14 13:28:47 +0800

    Update ListLayoutWithTags.tsx

--- layouts/ListLayoutWithTags.tsx
+++ layouts/ListLayoutWithTags.tsx
@@ -136,12 +136,12 @@ export default function ListLayoutWithTags({
           </h1>
           <div className="relative max-w-lg">
             <label>
-              <span className="sr-only">search posts :P</span>
+              <span className="sr-only">search posts</span>
               <input
-                aria-label="search posts :P"
+                aria-label="search posts"
                 type="text"
                 onChange={(e) => setSearchValue(e.target.value)}
-                placeholder="search posts :P"
+                placeholder="search posts"
                 className="block w-full rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-900 focus:border-sky-500 focus:ring-sky-500 dark:border-gray-900 dark:bg-gray-800 dark:text-gray-100"
               />
             </label>
